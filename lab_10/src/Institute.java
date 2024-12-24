c03468d7afc50d1cf5884595ee920866bc7313f1
imxenia <121744362+imxenia@users.noreply.github.com>
2024-12-24 23:01:25 +0300

    Add files via upload

---
+++ lab_10/src/Institute.java
@@ -0,0 +1,22 @@
+public class Institute {
+
+	public Institute(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+
+	public String name;
+	public String address;
+
+	public void open(){
+
+	}
+	public void close(){
+
+	}
+
+	public Integer showRating(){
+        return 100;
+	}
+
+}
